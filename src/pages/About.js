26e6e5e713d7e2577381736f18ea7bd9578a99ae
Muhammadamin Zakir <perseusp3@gmail.com>
2022-01-28 19:17:06 +0300

    React

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        deleniti accusantium similique dolorem iusto consequuntur unde, ratione
+        reprehenderit. Beatae veniam, non eaque hic iusto culpa rem voluptates
+        iste nobis corporis, voluptate vero amet vel? Nesciunt similique
+        assumenda iste numquam eum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
